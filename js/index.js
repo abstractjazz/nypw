88ec3f426e255c233a15addd952bba5b3109d5c5
github username <github email address>
2021-12-31 13:24:25 -0800

    adds additional form fields

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     setTimeout(()=>{addToHeadline()}, 3000)
+// })
+
+// const addToHeadline = () => {
+//     const h1 = document.querySelector('#mast-container > h1')
+//     h1.style.color="black"
+    
+// }
