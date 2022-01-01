518f6b7b7b7bfc0f18b5235362e9722c79bedd4a
github username <evan@heyevan.com>
2021-12-31 17:29:16 -0800

    adds menu & link animation

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@
 //     h1.style.color="black"
     
 // }
+
+
+function myFunction() {
+    const links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
