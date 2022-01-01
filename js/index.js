475add5aecfa9c8dd1a8098879a6cd044336ac2b
github username <evan@heyevan.com>
2021-12-31 18:21:30 -0800

    adds menu interaction

--- js/index.js
+++ js/index.js
@@ -10,10 +10,25 @@
 
 
 function myFunction() {
+    
     const links = document.getElementById("myLinks");
+    const blur = document.getElementById("blur-wrapper")
+    const nav = document.getElementById('nav')
     if (links.style.display === "block") {
       links.style.display = "none";
     } else {
       links.style.display = "block";
     }
-  }
+
+    if (links.style.display === "block") {
+      blur.style.filter="blur(9px)";
+      nav.style.height = "100vh";
+      nav.style.fontSize="3em";
+      } else {
+        blur.style.filter=""
+        nav.style.height=""
+        nav.style.fontSize="1.25em"
+      }
+    }
+
+    
